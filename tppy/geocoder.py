fb59d9cf0f24daf7aab38ac4e537e1e665cc6121
arikarchmer <ari.karchmer.s@gmail.com>
2017-07-12 20:46:25 -0400

    First Commit

---
+++ tppy/geocoder.py
@@ -0,0 +1,29 @@
+import urllib
+import json
+import logging
+import urllib2
+from geoKeys import Keys
+
+
+class Geocoder():
+
+    key = Keys.geo_key
+    geo_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+    def getCoordinates(self, city, state):
+        loc_string = city + '+' + state
+        url = Geocoder.geo_url + urllib.urlencode({'key': Geocoder.key, 'address': loc_string})
+        logging.info('url='+url)
+        response = urllib2.urlopen(url)
+        result = response.read()
+        j = json.loads(result)
+
+        return {'lat': j['results'][0]['geometry']['location']['lat'],
+                'lng': j['results'][0]['geometry']['location']['lng']}
+
+
+if __name__ == '__main__':
+
+    g = Geocoder()
+
+    print g.getCoordinates(('Sydney', 'Australia'))
