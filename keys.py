236faafacf8c940a16e4aef80207fb9b9db63206
Ari Karchmer <ari.karchmer.s@gmail.com>
2017-09-28 18:14:15 -0400

    Add files via upload
    keys template

---
+++ keys.py
@@ -0,0 +1,9 @@
+
+
+class keys:
+
+	consumer_key = 'YOUR CONSUMER KEY'
+	consumer_secret = 'YOUR CONSUMER SECRET'
+	access_token = 'YOUR ACCESS TOKEN'
+	access_token_secret = 'YOUR ACCESS TOKEN SECRET'
+    maps_key = 'YOUR GOOGLE API KEY'
